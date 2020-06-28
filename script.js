2af6dd6bc242cfc29634960fd888c81d9112c4b7
ricotomo <friedicecream1@gmail.com>
2020-06-29 00:11:50 +0200

    Script for toggle
    Wrote function to change the 5 reasons for studying in Euorpe depending on how the toggle switch is set (parent or student)

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ $(document).ready(function() {
         // Form Submit Handler
     });
 });
+
+// function to change the 5 reasons for studying in Euorpe depending on how the toggle switch is set (parent or student)
+function changeText() {
+    
+    var checkBox = document.getElementById("mySwitch");
+    
+    if (checkBox.checked == true){
+        document.getElementById("reason1").innerHTML = "New text 1!";
+        document.getElementById("reason2").innerHTML = "New text 2!";
+        document.getElementById("reason3").innerHTML = "New text 3!";
+        document.getElementById("reason4").innerHTML = "New text 4!";
+        document.getElementById("reason5").innerHTML = "New text 5!"; 
+    } else {
+        document.getElementById("reason1").innerHTML = "hello world 1";
+        document.getElementById("reason2").innerHTML = "hello world 2";
+        document.getElementById("reason3").innerHTML = "hello world 3";
+        document.getElementById("reason4").innerHTML = "hello world 4";
+        document.getElementById("reason5").innerHTML = "hello world 5";
+    }
+  }
+  
