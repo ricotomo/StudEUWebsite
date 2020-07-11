b8fb77959eb1d1295c0703339d5f6d3a4baa1bc5
ricotomo <friedicecream1@gmail.com>
2020-07-11 17:19:22 +0200

    text + ReCaptcha
    Minor changes in text. attempt at reCaptcha. testing s3 deployment pipeline.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
     $body.click(() => {
         $body.removeClass('show-nav');
     });
-
+// remove?
     $('.slider').slick({
         dots: true,
         infinite: true,
         prevArrow: '<button class="slick-prev button">&#10094;</button>',
         nextArrow: '<button class="slick-next button">&#10095;</button>'
     });
-
+// function to get slide in side menu on mobile
     $('#header-nav').click((e) => {
         e.stopPropagation();
         $body.removeClass('show-nav');
@@ -25,7 +25,7 @@ $(document).ready(function() {
     $('#header-nav-close').click(() => {
         $body.removeClass('show-nav');
     });
-
+// remove?
     $('.button-parent').click(() => {
         $('.tabs').removeClass('student').addClass('parent');
     });
@@ -36,7 +36,7 @@ $(document).ready(function() {
     $('#form').submit((e) => {
         e.preventDefault();
         
-        // Form Submit Handler
+        // Form Submit Handler- remove?
     });
 });
 
@@ -45,9 +45,9 @@ function prefillMessage(x){
     if (x === 'explore'){
         document.getElementById("form-message").value = "I would like to find out more about what educational opportunities exist for me in Europe. Please get in touch!";  
     } else if (x === 'apply') {
-        document.getElementById("form-message").value = "I want to go study in Europe, and I've already found the perfect program! I want your help to get accepted. Please get in touch! .";
+        document.getElementById("form-message").value = "I want to study in Europe, and I've already found the perfect program! I want your help to get accepted. Please get in touch! .";
     } else if (x === 'relocate'){
-        document.getElementById("form-message").value = "I found th eperfect study program in Europe, and have been accepted. I'm really excited to go! Now I need help planning my move. Please get in touch! ";
+        document.getElementById("form-message").value = "I found the perfect study program in Europe, and have been accepted. I'm really excited to go! Now I need help planning my move. Please get in touch! ";
     } else {
         document.getElementById("form-message").value = "I am interested in your services. Please get in touch!";
     }
@@ -62,16 +62,18 @@ function changeText() {
     var checkBox = document.getElementById("mySwitch");
     
     if (checkBox.checked == true){
+        // reasons for students
         document.getElementById("reason1").innerHTML = "<b>Culture</b> - By studying in Europe you'll get to truly experience first hand the epic history, delicious food, awestriking art and everything which makes Europe special. It's different but also familiar.";
         document.getElementById("reason2").innerHTML = "<b>Travel</b> -Take advantage of Europes excellent public transport and budget airlines to cruise the canals of Venice, admire the art of the Louvre, have a beer at Oktoberfest, or whatever else is on your bucket list!";
         document.getElementById("reason3").innerHTML = "<b>Education Style</b> - In European universities you usually don't get homework and there are no quizes. How does that sound?! Classes are smaller and familiar leading to lifelong friendships between classmates ";
-        document.getElementById("reason4").innerHTML = "<b> Partying</b> - Europe's drinking laws are much less restrictive than in the USA. Clubs and bars are open to anyone 18+ (even 16 in many countries).";
+        document.getElementById("reason4").innerHTML = "<b>Social Life</b> - Europe's laws are much less restrictive than in the USA regarding partying. Clubs and bars are open to anyone 18+ (even 16 in many countries).";
         document.getElementById("reason5").innerHTML = "<b>Independence</b> - European universities don't baby their students. If you're 18 you're an adult. That means no RA's, shared dorms, meal plans, attendance, etc - of course this come with much more responsibility."; 
     } else {
+        // reasons for parents
         document.getElementById("reason1").innerHTML = "<b>Fast</b> - A bachelor's degree in Europe only takes 3 years, and is internationally recognized. Get a head start on a career.";
-        document.getElementById("reason2").innerHTML = "<b>Competitive Boost</b> - Europe offers excellent academic and career opportunities. Becuase of a shrinking and aging population Europe is in need of foreigners. This gives you an extra boost in getting prestigious positions.";
-        document.getElementById("reason3").innerHTML = "<b>Value</b> - European university is much cheaper than in the USA. In many countries there is no tuition fee. The cost of living is also cheaper (with a few exceptions in Scandanavia).";
-        document.getElementById("reason4").innerHTML = "<b>Diversity</b> - Expose your child to a diversity of culture. Experiencing new languages, places, and people is garaunteed to broaden horizons.";
+        document.getElementById("reason2").innerHTML = "<b>Competitive Boost</b> - Europe offers excellent academic and career opportunities. Because of a shrinking and aging population Europe is welcoming to foreigners. This gives you an extra boost in getting prestigious positions.";
+        document.getElementById("reason3").innerHTML = "<b>Excellent Value</b> - European university is much cheaper than in the USA. In many countries there is no tuition fee. The cost of living is also cheaper (with a few exceptions in Scandanavia).";
+        document.getElementById("reason4").innerHTML = "<b>Diversity</b> - Give your child the chance to experience the diversity of cultures in Europe! Experiencing new languages, places, and people is garaunteed to broaden horizons.";
         document.getElementById("reason5").innerHTML = "<b>Language</b> - Learning to speak a second language can open a world of new possibilites.";
     }
   }
@@ -144,7 +146,8 @@ function blog(){
  
         $.ajax({
           type: "POST",
-          url : "https://8yq7hi2oli.execute-api.us-east-2.amazonaws.com/Alpha/contact",
+          //url : "https://8yq7hi2oli.execute-api.us-east-2.amazonaws.com/Alpha/contact",
+          url : "https://api.studeu.net/contact",
           dataType: "json",
           crossDomain: "true",
           contentType: "application/json; charset=utf-8",
@@ -160,7 +163,9 @@ function blog(){
           },
           error: function () {
             // show an error message
+            console.log(Error.prototype.message + "my error");
             document.getElementById("sent-message").innerHTML = "Oops there was a problem. Try again or get in touch by email!";
             setTimeout(unshowMessage, 4000);
           }});
       }
+
